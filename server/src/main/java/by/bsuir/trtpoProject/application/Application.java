ec9626f141eb2604dfeb6e386aad8e94bc6bd8c9
Dima Alhovik <halapsikon@mail.ru>
2020-03-10 00:57:49 +0300

    add example

--- server/src/main/java/by/bsuir/trtpoProject/application/Application.java
+++ server/src/main/java/by/bsuir/trtpoProject/application/Application.java
@@ -1,4 +1,4 @@
-package by.bsuir.trtpoProject.controller;
+package by.bsuir.trtpoProject.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
