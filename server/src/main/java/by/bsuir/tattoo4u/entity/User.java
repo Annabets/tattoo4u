b130738c5f6aca213013708f67c48da7109bd882
JFreshek <48886204+JFreshek@users.noreply.github.com>
2020-03-09 21:17:07 +0300

    Add Post and User entity

---
+++ server/src/main/java/by/bsuir/tattoo4u/entity/User.java
@@ -0,0 +1,88 @@
+package by.bsuir.tattoo4u.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String userName;
+
+    private String password;
+
+    private String email;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Post> posts;
+
+    public User() {
+    }
+
+    public User(String userName, String password, String email, Set<Post> posts) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.posts = posts;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(Set<Post> posts) {
+        this.posts = posts;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id) &&
+                userName.equals(user.userName) &&
+                password.equals(user.password) &&
+                email.equals(user.email) &&
+                posts.equals(user.posts);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userName, password, email, posts);
+    }
+}
