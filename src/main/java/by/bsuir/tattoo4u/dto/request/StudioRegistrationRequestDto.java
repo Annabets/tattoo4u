bd2c7325d675350854a8ec1cfb51793fc1b61b47
DavidMitsko <45606856+DavidMitsko@users.noreply.github.com>
2020-03-31 00:30:48 +0300

    Cleaning code

--- src/main/java/by/bsuir/tattoo4u/dto/request/StudioRegistrationRequestDto.java
+++ src/main/java/by/bsuir/tattoo4u/dto/request/StudioRegistrationRequestDto.java
@@ -4,34 +4,15 @@ import by.bsuir.tattoo4u.entity.Studio;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-//import by.bsuir.tattoo4u.entity.Contact;
-
-
 @Data
 @EqualsAndHashCode()
 public class StudioRegistrationRequestDto {
-    //private final static Gson gson = new Gson();
-
     private String name;
     private String description;
     private String address;
     private String contact;
 
-
-
     public Studio getStudio() {
         return new Studio(name, description, address, contact);
     }
-//
-//    public static StudioRegistrationRequestDto fromJson(String jsonString){
-//        if(jsonString==null || jsonString.isEmpty()){
-//            throw new EmptyDataException();
-//        }
-//        try {
-//            return gson.fromJson(jsonString, StudioRegistrationRequestDto.class);
-//        }catch (JsonSyntaxException e){
-//            throw new IncorrectDataInputException("Incorrect JSON object");
-//        }
-//
-//    }
 }
