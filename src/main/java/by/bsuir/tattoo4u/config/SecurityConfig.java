004f3366e138daf46416eb2df8e5ba70d82114b4
Dima Alhovik <halapsikon@mail.ru>
2020-03-31 00:42:29 +0300

    some fixes

--- src/main/java/by/bsuir/tattoo4u/config/SecurityConfig.java
+++ src/main/java/by/bsuir/tattoo4u/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().disable()
                 .httpBasic().disable()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
