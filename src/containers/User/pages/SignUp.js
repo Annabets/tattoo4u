5b5a74c7ac9ad6deb73577471a3f06abe2f4a2d3
Kate <ekaterina.saplicza@mail.ru>
2020-03-31 19:38:37 +0300

    add masters registration

--- src/containers/User/pages/SignUp.js
+++ src/containers/User/pages/SignUp.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Form, Button} from "react-bootstrap";
+import {Form, Button, Nav} from "react-bootstrap";
 import {signUp} from '../actions'
 import bgImg from '../../../assets/images/tattoo.jpg';
 import {connect} from "react-redux";
@@ -26,56 +26,71 @@ class SignUp extends React.Component {
     }
   };
 
+  topNav = () => (
+    <Nav fill variant="pills" defaultActiveKey="USER" onSelect={eventKey => this.setState({role: eventKey})}>
+      <Nav.Item>
+        <Nav.Link eventKey="USER">User</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="MATER">Tattoo master</Nav.Link>
+      </Nav.Item>
+    </Nav>
+  )
+
   render() {
-    const {username, password, confirmPassword, email } = this.state;
+    const {username, password, confirmPassword, email, role} = this.state;
     return (
-      <div className="sign-in" style={{backgroundImage: `url(${bgImg})`}}>
-        <div className="form-container">
-          <div className="form-content">
-            <h2 className="form-title">Sign Up as User</h2>
-            <Form onSubmit={this.handleSubmit}>
-              <Form.Group>
-                <Form.Control
-                  type="text"
-                  placeholder="Username"
-                  value={username}
-                  onChange={e => this.setState({username: e.target.value})}
-                />
-              </Form.Group>
-              <Form.Group>
-                <Form.Control
-                  type="email"
-                  placeholder="Email"
-                  value={email}
-                  onChange={e => this.setState({email: e.target.value})}
-                />
-              </Form.Group>
-              <Form.Group>
-                <Form.Control
-                  type="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={e => this.setState({password: e.target.value})}
-                />
-              </Form.Group>
-              <Form.Group>
-                <Form.Control
-                  type="password"
-                  placeholder="Confirm Password"
-                  value={confirmPassword}
-                  onChange={e => this.setState({confirmPassword: e.target.value})}
-                />
-              </Form.Group>
-              <Button variant="primary" type="submit" block className="form-btn">
-                Sign Up
-              </Button>
-              <Form.Text className="text-secondary mt-2">
-                Or <a href={SIGN_IN}>sign in now!</a>
-              </Form.Text>
-            </Form>
+      <>
+        <div className="sign-in" style={{backgroundImage: `url(${bgImg})`}}>
+          <div className="form-container">
+            <div className="form-content">
+              {this.topNav()}
+              <h2 className="form-title">Sign Up as {role === "USER" ? 'User' : 'Master'}</h2>
+              <Form onSubmit={this.handleSubmit}>
+                <Form.Group>
+                  <Form.Control
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={e => this.setState({username: e.target.value})}
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Control
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={e => this.setState({email: e.target.value})}
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={e => this.setState({password: e.target.value})}
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Control
+                    type="password"
+                    placeholder="Confirm Password"
+                    value={confirmPassword}
+                    onChange={e => this.setState({confirmPassword: e.target.value})}
+                  />
+                </Form.Group>
+                <Button variant="primary" type="submit" block className="form-btn">
+                  Sign Up
+                </Button>
+                <Form.Text className="text-secondary mt-2">
+                  Or <a href={SIGN_IN}>sign in now!</a>
+                </Form.Text>
+              </Form>
+            </div>
           </div>
         </div>
-      </div>
+
+      </>
     )
   }
 }
--- src/containers/User/reducer.js
+++ src/containers/User/reducer.js
@@ -1,5 +1,5 @@
 import * as constants from './constants';
-import {AUTH_KEY, USER_NAME} from "../../constants";
+import {AUTH_KEY, EMAIL, USER_NAME} from "../../constants";
 
 const initialState = {
   id: '',
@@ -13,16 +13,13 @@ const initialState = {
 export function userReducer(state = initialState, action) {
   switch (action.type) {
     case constants.SIGN_IN_USER_REQUEST:
-      return {...state, isLoadingUserData: true};
-    case constants.SIGN_IN_USER_SUCCESS:
-      localStorage.setItem(AUTH_KEY, action.userData.token);
-      localStorage.setItem(USER_NAME, action.userData.username);
-      return {...state, isLoadingUserData: false, username: action.userData.username, authKey: action.userData.token};
-    case constants.SIGN_IN_USER_FAILURE:
-      return {isLoadingUserData: false, error: action.payload};
     case constants.SIGN_UP_USER_REQUEST:
       return {...state, isLoadingUserData: true};
-    case constants.SIGN_UP_USER_SUCCESS:
+    case constants.SIGN_IN_USER_SUCCESS:
+      case constants.SIGN_UP_USER_SUCCESS:
+      localStorage.setItem(AUTH_KEY, action.userData.token);
+      localStorage.setItem(USER_NAME, action.userData.username);
+      localStorage.setItem(EMAIL, action.userData.email);
       return {
         ...state,
         isLoadingUserData: false,
@@ -31,10 +28,12 @@ export function userReducer(state = initialState, action) {
         username: action.userData.username
       };
     case constants.SIGN_UP_USER_FAILURE:
+    case constants.SIGN_IN_USER_FAILURE:
       return {...state, isLoadingUserData: false, error: action.payload};
     case constants.SIGN_OUT_USER:
       localStorage.removeItem(AUTH_KEY);
       localStorage.removeItem(USER_NAME);
+      localStorage.removeItem(EMAIL);
       return initialState;
     default:
       return state;
