4eeadb096035f1a99a884144e18cd8be71a8230c
Kate <ekaterina.saplicza@mail.ru>
2020-04-21 23:11:51 +0300

    add photos api methods

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,11 @@
-import {API_URL, SIGN_IN, SIGN_UP, GET, POST} from "../constants";
+import {API_URL, SIGN_IN, SIGN_UP, GET, POST, DELETE} from "../constants";
 import axios from 'axios';
 import {getToken} from "../utils";
 
 const handleResponse = resp =>
   resp.text()
     .then(text => resp.ok ? Promise.resolve(JSON.parse(text)) : Promise.reject(text))
-    .catch(error => Promise.reject(error.message));
+    .catch(error => Promise.reject(JSON.parse(error)));
 
 
 const signInUser = data => fetch(`${API_URL + SIGN_IN}`, {method: POST, headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)}).then(handleResponse);
@@ -30,6 +30,12 @@ const uploadPhoto = (file, tags, description) => {
 
 const getPhotos = () => fetch(`${API_URL + '/posts'}`, {method: GET, headers: {'Authorization':`Bearer_${getToken()}`}}).then(handleResponse)
 
+const deletePhoto = photoId => fetch(API_URL + `/delete-post/${photoId}`, {method: DELETE, headers: {'Authorization':`Bearer_${getToken()}`}}).then(handleResponse)
+
+const getMasterPhotos = masterId => fetch(API_URL + `/take-posts/${masterId}`, {method: GET, headers: {'Authorization':`Bearer_${getToken()}`}}).then(handleResponse)
+
+const searchPhotos = tags => fetch(API_URL + `/take-posts?tags=${tags}`, {method: GET}).then(handleResponse)
+
 const getStudios = searchString => fetch(`${API_URL + '/studios' + (searchString ? `?name=${searchString}` : '')}`, {method: GET}).then(handleResponse);
 
 const getStudioData = studioId => fetch(API_URL + `/studio?studioId=${studioId}`, {method: GET}).then(handleResponse);
@@ -43,7 +49,7 @@ const registerStudio = data => fetch(`${API_URL + '/studio'}`, {
   body: JSON.stringify(data)
 }).then(handleResponse);
 
-const getMasters = searchString => fetch(`${API_URL + '/masters' + (searchString ? `?name=${searchString}` : '')}`, {method: GET, headers:{'Authorization':`Bearer_${getToken()}`}}).then(handleResponse);
+const getMasters = searchString => fetch(`${API_URL + '/masters' + (searchString ? `?name=${searchString}` : '')}`, {method: GET}).then(handleResponse);
 
 const registerMaster = data => fetch(API_URL + '/master', {
   method: POST,
@@ -77,5 +83,8 @@ export const api = {
   registerStudio,
   getCurrentUser,
   getMasters,
-  registerMaster
+  registerMaster,
+  deletePhoto,
+  getMasterPhotos,
+  searchPhotos
 };
